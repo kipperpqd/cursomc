8982fcabfffb1b4a5ea9942a2a87e2abc37a6b43
kipperpqd <spylma@gmail.com>
2018-07-05 22:43:43 -0300

    Ajustes no end point categorias/id

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired 
-	/* @Autowired instancia automaticamente a dependencia repo 
-	* pelo mecanismo de injeção de dependencias ou inversao de controle*/
 	private CategoriaRepository repo;
 	
 	public Categoria buscar (Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				
+		
 	}
 }
